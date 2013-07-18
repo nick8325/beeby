0b09acc0ad4b36745327189ef866d2be2eaad5b7
Nick Smallbone <nick.smallbone@gmail.com>
2013-07-18 15:42:37 +0200

    debugging

--- Main.hs
+++ Main.hs
@@ -3,32 +3,26 @@ module Main(main) where
 
 import Six502.Interpreter
 import Six502.CPU
-import Six502.Machine
 import BBC.Machine
 import Six502.Memory
 import Control.Monad hiding (forever)
-import Numeric
+import Six502.Machine
 
+x `inRange` (y, z) = x >= y && x <= z
+
+{-# INLINE hook #-}
 hook ram = do
   pc <- liftM fromAddr loadPC
-  let syscall f = liftM fromByte (peek (register A)) >>= liftIO . f
-      osword 0 = putStrLn "read line" >> dumpScreen ram
-      osbyte 0x83 = putStrLn "83"
-      osbyte 0x84 = putStrLn "84"
-      oswrch f = putStrLn $ "wrch " ++ showHex f ""
-      oswrch2 _ = putStrLn "wrch2"
-  case pc of
-    0xfff1 -> syscall osword
-    0xfff4 -> syscall osbyte
-    0xffee -> syscall oswrch
-    0xe0a4 -> liftM fromByte (peek (memory (address 0x27c))) >>= liftIO . oswrch
-    0xe0c1 -> liftM fromByte (peek (memory (address 0x27c))) >>= liftIO . oswrch
-    0xe0c3 -> syscall oswrch2
-    _ -> return ()
+  when (pc `elem` [0xfff1, 0xfff4, 0xffee, 0xe0a4, 0xe0c1, 0xe0c3] ||
+        pc `inRange` (0xdb76, 0xdb7f)) $
+    gets id >>= liftIO . print
+
   n <- gets ticks
-  when (n >= 10000000) $ do
+  when (n >= 1000000) $ do
+    replicateM 10 $ do
+      cpu
+      gets id >>= liftIO . print
     liftIO (dumpScreen ram)
-    liftM fromByte (peek (memory (address 0x27c))) >>= liftIO . print
     error "Finished!"
 
 main = do
